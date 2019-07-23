dc9135f0ba19c8246817a22375bcfbe63b4af956
王红庆 <272338444@qq.com>
2019-07-23 17:28:42 +0800

    2.1.7版本测试成功
    2.1.7版本测试成功

--- TestAnimateDemo/TestAnimateDemo/Classes/News/Controller/QQNewsViewController.m
+++ TestAnimateDemo/TestAnimateDemo/Classes/News/Controller/QQNewsViewController.m
@@ -74,21 +74,41 @@
         _tableView.rowHeight = 100;
         [self.view addSubview:_tableView];
 
+//        // 设置tabAnimated相关属性
+//        // 可以不进行手动初始化，将使用默认属性
+//        _tableView.tabAnimated = [TABTableAnimated animatedWithCellClass:[QQNewsCell class]
+//                                                              cellHeight:100];
+//        _tableView.tabAnimated.animatedSectionCount = 3;
+//        _tableView.tabAnimated.showTableHeaderView = YES;
+//        _tableView.tabAnimated.categoryBlock = ^(UIView * _Nonnull view) {
+//            view.animation(1).down(3).height(12);
+//            view.animation(2).height(12).width(110);
+//            view.animation(3).down(-5).height(12);
+//        };
+//
+//        // 静态头视图扩展回调使用方法
+//        _tableView.tableHeaderView.tabAnimated.categoryBlock = ^(UIView * _Nonnull view) {
+//
+//        };
+        
         // 设置tabAnimated相关属性
         // 可以不进行手动初始化，将使用默认属性
         _tableView.tabAnimated = [TABTableAnimated animatedWithCellClass:[QQNewsCell class]
-                                                              cellHeight:100];
+                                                              cellHeight:90];
         _tableView.tabAnimated.animatedSectionCount = 3;
         _tableView.tabAnimated.showTableHeaderView = YES;
-        _tableView.tabAnimated.categoryBlock = ^(UIView * _Nonnull view) {
-            view.animation(1).down(3).height(12);
-            view.animation(2).height(12).width(110);
-            view.animation(3).down(-5).height(12);
+        _tableView.tabAnimated.superAnimationType = TABViewSuperAnimationTypeDrop;
+        
+        // 新回调
+        _tableView.tabAnimated.adjustBlock = ^(TABComponentManager * _Nonnull manager) {
+            manager.animation(1).down(3).height(12);
+            manager.animation(2).height(12).width(110);
+            manager.animation(3).down(-5).height(12);
         };
-
-        // 静态头视图扩展回调使用方法
-        _tableView.tableHeaderView.tabAnimated.categoryBlock = ^(UIView * _Nonnull view) {
-
+        
+        // 静态头视图扩展新回调使用方法
+        _tableView.tableHeaderView.tabAnimated.adjustBlock = ^(TABComponentManager * _Nonnull manager) {
+            
         };
     }
     return _tableView;
